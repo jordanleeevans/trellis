354943de64847ff8b0800c22fb5d5053131ec2ba
Jordan Evans <jordanleeevans@hotmail.com>
2026-09-12 13:21:50 +1200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use crossterm::event;
+
+fn main() -> std::io::Result<()> {
+    ratatui::run(|terminal| {
+        loop {
+            terminal.draw(|frame| frame.render_widget("Hello World!", frame.area()))?;
+            if event::read()?.is_key_press() {
+                break Ok(());
+            }
+        }
+    })
+}
